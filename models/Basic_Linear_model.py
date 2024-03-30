73f62884021b9df1066e14bcae2fef1bdf7616b1
AI 공부 <103333989+Josoobeen@users.noreply.github.com>
2024-03-30 16:01:32 +0900

    Update Basic_Linear_model.py

--- models/Basic_Linear_model.py
+++ models/Basic_Linear_model.py
@@ -1,3 +1,12 @@
-class Dense():
+class Linear():
+  def __init__(self, a, b):
+    self.alpha = a
+    self.beta = b
+
+  def train(self, x, y):
+    y_ = self.alpha * x + self.beta
+    loss_alpha = y_ - y
+    
+  
 
 
